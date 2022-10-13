3e689f1a4b4d3e5dac929c1c1a0b8ac18032cccb
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-10-13 16:34:24 -0300

    adding and configuring stitches

---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from '@stitches/react';
+
+export const {
+  config,
+  styled,
+  css,
+  globalCss,
+  getCssText,
+  keyframes,
+  theme,
+  createTheme
+} = createStitches({
+  theme: {
+    colors: {
+      
+    }
+  }
+});
