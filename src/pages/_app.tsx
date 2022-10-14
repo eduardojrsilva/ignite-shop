7e1c780ce6eff4fed2eae330727081f2b6ad5066
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-10-14 13:28:50 -0300

    home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app";
+import Image from 'next/future/image';
 import { globalStyles } from "../styles/global";
 
 import logoImg from '../assets/logo.svg';
@@ -10,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg.src} alt="" width={130} height={50} />
       </Header>
 
       <Component {...pageProps} />
